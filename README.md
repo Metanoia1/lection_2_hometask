612a68f9a47d969f4ffb442a3ef830f3ecc76f45
metanoia <meta3479@gmail.com>
2021-06-06 22:50:41 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Light IT - Lection 2 (hometask)
+
+### Start Guide:
+
+1. on Ubuntu
+```
+sudo apt install python3-tk
+```
+
+2. Run the following command in your command line:
+```
+python sierpinski_triangle.py
+```
---
+++ sierpinski_triangle.py
@@ -0,0 +1,57 @@
+import turtle
+
+
+def draw_triangle(order=1, step=25, angle=-60, turn=1):
+
+    if order < 0:
+        return
+
+    if turn == 1:
+        move = turtle.left
+
+    if turn == -1:
+        move = turtle.right
+
+    draw_triangle(order - 1, step=step, angle=angle, turn=-turn)
+    turtle.forward(step)
+    move(angle)
+    draw_triangle(order - 1, step=step, angle=-angle, turn=-turn)
+    turtle.forward(step)
+    move(angle)
+    draw_triangle(order - 1, step=step, angle=angle, turn=-turn)
+
+
+def draw_triangle_in_one_direction(order=1, step=25, angle=-60, turn=1):
+
+    if order % 2 == 0:
+        turtle.setheading(60)
+    else:
+        turtle.setheading(0)
+
+    draw_triangle(order=order, step=step, angle=angle, turn=turn)
+
+
+def main(order=1, step=100, angle=-60, turn=1, x=-250, y=-250):
+
+    turtle.tracer(1, 0)
+    step = step / (2 ** (order - 1))
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    draw_triangle_in_one_direction(order=order, step=step, angle=angle, turn=turn)
+    turtle.forward(step)
+    turtle.done()
+
+
+if __name__ == "__main__":
+    ORDER = 0
+
+    while ORDER <= 0:
+        ORDER = input("Enter the 'ORDER' value: ")
+
+        if ORDER.isdigit():
+            ORDER = int(ORDER)
+        else:
+            ORDER = 0
+
+    main(order=ORDER)
